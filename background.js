e6bfa4dad8e5c6b475093decc8d538c8532746b0
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-20 19:35:59 +0200

    make more robust

--- background.js
+++ background.js
@@ -1,47 +1,49 @@
 function updateTabNumbers() {
-    chrome.tabs.query({}, (tabs) => {
-      const tabCount = tabs.length;
-      tabs.forEach((tab, index) => {
-        let tabNumber;
-        if (index < 8) {
-          tabNumber = index + 1;
-        } else if (index === tabCount - 1) {
-          tabNumber = 9;
-        } else {
-          tabNumber = null;
-        }
-        
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          func: addTabNumber,
-          args: [tabNumber]
-        });
+  chrome.tabs.query({}, (tabs) => {
+    const tabCount = tabs.length;
+    tabs.forEach((tab, index) => {
+      let tabNumber;
+      if (index < 8) {
+        tabNumber = index + 1;
+      } else if (index === tabCount - 1) {
+        tabNumber = 9;
+      } else {
+        tabNumber = null;
+      }
+
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: addTabNumber,
+        args: [tabNumber]
       });
     });
-  }
-  
-  function addTabNumber(tabNumber) {
-    const tabTitle = document.title;
-    const regex = /^\[\d+\] /;
-    if (tabNumber !== null) {
-      if (!tabTitle.startsWith(`[${tabNumber}] `)) {
-        document.title = `[${tabNumber}] ` + tabTitle.replace(regex, '');
-      }
-    } else {
-      document.title = tabTitle.replace(regex, '');
-    }
-  }
-  
-  chrome.tabs.onCreated.addListener(updateTabNumbers);
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
-      updateTabNumbers();
-    }
   });
-  chrome.tabs.onRemoved.addListener(updateTabNumbers);
-  chrome.tabs.onMoved.addListener(updateTabNumbers);
-  chrome.tabs.onDetached.addListener(updateTabNumbers);
-  chrome.tabs.onAttached.addListener(updateTabNumbers);
-  
-  chrome.runtime.onInstalled.addListener(updateTabNumbers);
-  
+}
+
+function addTabNumber(tabNumber) {
+  const tabTitle = document.title;
+  const regex = /^\[\d+\] /;
+  if (tabNumber !== null) {
+    if (!tabTitle.startsWith(`[${tabNumber}] `)) {
+      document.title = `[${tabNumber}] ` + tabTitle.replace(regex, '');
+    }
+  } else {
+    document.title = tabTitle.replace(regex, '');
+  }
+}
+
+// Update tab numbers on various tab events
+chrome.tabs.onCreated.addListener(updateTabNumbers);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' || changeInfo.title) {
+    updateTabNumbers();
+  }
+});
+chrome.tabs.onRemoved.addListener(updateTabNumbers);
+chrome.tabs.onMoved.addListener(updateTabNumbers);
+chrome.tabs.onDetached.addListener(updateTabNumbers);
+chrome.tabs.onAttached.addListener(updateTabNumbers);
+chrome.tabs.onActivated.addListener(updateTabNumbers);
+
+chrome.runtime.onInstalled.addListener(updateTabNumbers);
+chrome.windows.onFocusChanged.addListener(updateTabNumbers);
--- content.js
+++ content.js
@@ -1,15 +1,14 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "updateTabNumber") {
-      const { tabNumber } = message;
-      const tabTitle = document.title;
-      const regex = /^\[\d+\] /;
-      if (tabNumber !== null) {
-        if (!tabTitle.startsWith(`[${tabNumber}] `)) {
-          document.title = `[${tabNumber}] ` + tabTitle.replace(regex, '');
-        }
-      } else {
-        document.title = tabTitle.replace(regex, '');
+  if (message.action === "updateTabNumber") {
+    const { tabNumber } = message;
+    const tabTitle = document.title;
+    const regex = /^\[\d+\] /;
+    if (tabNumber !== null) {
+      if (!tabTitle.startsWith(`[${tabNumber}] `)) {
+        document.title = `[${tabNumber}] ` + tabTitle.replace(regex, '');
       }
+    } else {
+      document.title = tabTitle.replace(regex, '');
     }
-  });
-  
+  }
+});
